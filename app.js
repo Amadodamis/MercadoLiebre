fbb7bc817ec64e2198565013ddc7a5961bf8db8a
Amado <amadodamis@gmail.com>
2021-09-28 12:19:48 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const path=require("path");
+app.use(express.static("public"));
+
+app.listen(3232,()=>{
+    console.log("Servidor online");
+    console.log(">>>>>>>>>>>>>>>>>>>>>>>","http://localhost:3232/", "<<<<<<<<<<<<<<<<<<<<<<<<");
+    console.log(">>>>>>>>>>>>>>>>>>>>>>>","http://192.168.0.145:3232", "<<<<<<<<<<<<<<<<<<<<<<<<");
+});
+
+app.get("/", (req, res)=>{
+    const fileToSend=path.join(__dirname,"/views/index.html")
+    res.sendFile(fileToSend)
+})
+
+app.get("/register", (req, res)=>{
+    const fileToSend=path.join(__dirname,"/views/register.html")
+    res.sendFile(fileToSend)
+})
+
+app.get("/login", (req, res)=>{
+    const fileToSend=path.join(__dirname,"/views/login.html")
+    res.sendFile(fileToSend)
+})
+
+
+app.post("/register",(req,res)=>{
+    res.redirect("/")
+})
+
+app.post("/login",(req,res)=>{
+    res.redirect("/")
+})
